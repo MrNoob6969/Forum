dc6b62e84ca2ea8c472a804ef6a550a84bfe27d5
MrNoob6969 <45920022+MrNoob6969@users.noreply.github.com>
2018-12-24 21:37:04 +0400

    Update credentials.js

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
-  GITHUB_CLIENT_ID: 'fc125763a00c81dd3aeb',
-  GITHUB_CLIENT_SECRET: '1523526c34ccbab8f4d5971249751068696d1301',
+  GITHUB_CLIENT_ID: '548eb82e388774996556',
+  GITHUB_CLIENT_SECRET: '63de46572d84eb99080f18fe5cc35cf7267ec090',
   GITHUB_CALLBACK_URL: 'https://skynetmc.herokuapp.com/api/user/authViaGitHub/callback',
   DBURL: 'mongodb://kem:yeshna12@ds243254.mlab.com:43254/skynet',
 };
